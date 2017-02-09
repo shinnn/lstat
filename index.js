aca0747a597521c451a27b83dd7689629f35842f
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-02-09 18:28:57 +0900

    remove fallback for the environment where Promise doesn't exist
    [BREAKING] drop support for Node.js v0.10

--- index.js
+++ index.js
@@ -6,23 +6,18 @@
 
 var inspect = require('util').inspect;
 
-var PinkiePromise = require('pinkie-promise');
 var fsLstat = require('graceful-fs').lstat;
 
 module.exports = function lstat(path) {
   if (typeof path !== 'string') {
-    return PinkiePromise.reject(new TypeError(
-      'Expected a file path (string), but got a non-string value ' +
-      inspect(path) +
-      '.'
-    ));
+    return Promise.reject(new TypeError('Expected a file path (string), but got a non-string value ' + inspect(path) + '.'));
   }
 
   if (path.length === 0) {
-    return PinkiePromise.reject(new Error('Expected a file path, but got \'\' (empty string).'));
+    return Promise.reject(new Error('Expected a file path, but got \'\' (empty string).'));
   }
 
-  return new PinkiePromise(function(resolve, reject) {
+  return new Promise(function(resolve, reject) {
     fsLstat(path, function(err, result) {
       if (err) {
         reject(err);
