f7d3c31a465ce3985c1be576b73ab7f8b99a9423
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-02-11 10:46:59 +0900

    use const, template literals and arrow function
    [BREAKING] drop support for Node.js < 4

--- index.js
+++ index.js
@@ -4,21 +4,21 @@
 */
 'use strict';
 
-var inspect = require('util').inspect;
+const inspect = require('util').inspect;
 
-var fsLstat = require('graceful-fs').lstat;
+const fsLstat = require('graceful-fs').lstat;
 
 module.exports = function lstat(path) {
   if (typeof path !== 'string') {
-    return Promise.reject(new TypeError('Expected a file path (string), but got a non-string value ' + inspect(path) + '.'));
+    return Promise.reject(new TypeError(`Expected a file path (string), but got a non-string value ${inspect(path)}.`));
   }
 
   if (path.length === 0) {
     return Promise.reject(new Error('Expected a file path, but got \'\' (empty string).'));
   }
 
-  return new Promise(function(resolve, reject) {
-    fsLstat(path, function(err, result) {
+  return new Promise((resolve, reject) => {
+    fsLstat(path, (err, result) => {
       if (err) {
         reject(err);
         return;
