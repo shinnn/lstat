938e29acfcd437e8b2391843bfc8aa25b27e6350
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-02-16 10:46:59 +0900

    use destructuring assignment
    and some minor refactoring
    
    [BREAKING] drop support for Node.js < 6

--- index.js
+++ index.js
@@ -4,20 +4,20 @@
 */
 'use strict';
 
-const inspect = require('util').inspect;
+const {inspect} = require('util');
 
 const fsLstat = require('graceful-fs').lstat;
 
 module.exports = function lstat(path) {
-  if (typeof path !== 'string') {
-    return Promise.reject(new TypeError(`Expected a file path (string), but got a non-string value ${inspect(path)}.`));
-  }
-
-  if (path.length === 0) {
-    return Promise.reject(new Error('Expected a file path, but got \'\' (empty string).'));
-  }
-
   return new Promise((resolve, reject) => {
+    if (typeof path !== 'string') {
+      throw new TypeError(`Expected a file path (string), but got a non-string value ${inspect(path)}.`);
+    }
+
+    if (path.length === 0) {
+      throw new Error('Expected a file path, but got \'\' (empty string).');
+    }
+
     fsLstat(path, (err, result) => {
       if (err) {
         reject(err);
